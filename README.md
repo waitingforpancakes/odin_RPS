3a6e5d1d42df2423bcaa86c0ba7d0c6e0a5ee56a
Grace <greyse.leung@mail.utoronto.ca>
2022-01-03 01:18:28 -0500

    rps ui

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # odin_RPS
-Coding Rock, Paper, Scissors from scratch.
+Coding Rock, Paper, Scissors from scratch. Go to https://waitingforpancakes.github.io/odin_RPS/ to play.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay () {
     let options = ["Rock", "Paper", "Scissors"];
     return options[Math.floor(Math.random() * options.length)];
@@ -26,7 +29,7 @@ function playRound (playerSelection, computerSelection) {
         }
     } else if (playerSelection == "scissors") {
         if (computerSelection == "rock") {
-            return "You lose! Scissors beats Rock";
+            return "You lose! Rock beats Scissors";
         } else if (computerSelection == "paper") {
             return "You win! Scissors beats Paper";
         } else {
@@ -37,17 +40,15 @@ function playRound (playerSelection, computerSelection) {
     }
 }
 
-function game () {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++){
-        // Play round
-        let playerSelection = prompt("Make you Move: ");
-        let computerSelection = computerPlay();
-        let result = playRound(playerSelection, computerSelection);
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener('click', function() {
+        if (!this.textContent) return;
 
-        // Display round result
-        console.log(result);
+        // Play a round
+        const scoreContainer = document.querySelector("#score");
+        const resultContainer = document.querySelector("#results");
+        const result = playRound(this.textContent, computerPlay());
 
         // Update scores
         if (result.includes("win")) {
@@ -58,16 +59,25 @@ function game () {
         } else {
             computerScore++;
         }
-    }
+        scoreContainer.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+        resultContainer.textContent = result;
 
-    // Announce winner
-    if (playerScore == computerScore) {
-        console.log("It's a tie!");
-    } else if (playerScore > computerScore) {
-        console.log("Player Wins!");
-    } else {
-        console.log("Computer Wins!");
-    }
-}
-
-game();
+        // Announce Winner
+        if (playerScore >= 5 && computerScore >= 5) {
+            scoreContainer.textContent = "IT'S A TIE! CLICK A BUTTON TO PLAY AGAIN";
+            resultContainer.textContent = "IT'S A TIE! CLICK A BUTTON TO PLAY AGAIN";
+            playerScore = 0;
+            computerScore = 0;
+        } else if (playerScore >= 5){
+            scoreContainer.textContent = "YOU WIN! CLICK A BUTTON TO PLAY AGAIN";
+            resultContainer.textContent = "YOU WIN! CLICK A BUTTON TO PLAY AGAIN";
+            playerScore = 0;
+            computerScore = 0;
+        } else if (computerScore >= 5) {
+            scoreContainer.textContent = "YOU LOSE! CLICK A BUTTON TO PLAY AGAIN"
+            resultContainer.textContent = "YOU LOSE! CLICK A BUTTON TO PLAY AGAIN";
+            playerScore = 0;
+            computerScore = 0;
+        } 
+    });
+});
